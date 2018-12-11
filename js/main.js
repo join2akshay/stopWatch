10eb20f21f54690ac9b4e8344a69e168062384de
tvibu12 <tvibhu12@gmail.com>
2018-12-11 18:52:14 +0530

    add JQuery file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+//variables
+let mode=false;     //App mode
+let timeCounter=0;    //time Counter
+let lapCounter=0;     //Lap Counter
+let action;           //variable for setInterval
+let lapNumber=0;     //Number of Lap
+$('#start').click(function (e) { 
+    e.preventDefault();
+    
+});    
+});
