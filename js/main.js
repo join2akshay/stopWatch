8d4d34555df0eac33f7af2680f09981b12a61812
tvibu12 <tvibhu12@gmail.com>
2018-12-14 19:37:36 +0530

    update index.html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,62 @@
 
 
 $(document).ready(function () {
-//variables
-let mode=false;     //App mode
-let timeCounter=0;    //time Counter
-let lapCounter=0;     //Lap Counter
-let action;           //variable for setInterval
-let lapNumber=0;     //Number of Lap
-$('#start').click(function (e) { 
-    e.preventDefault();
+    //variables
+    let mode = 0; //App mode
+    let timeCounter = 0; //time Counter
+    let lapCounter = 0; //Lap Counter
+    let action; //variable for setInterval
+    let lapNumber = 0; //Number of Lap
+    let timeMinute,timeSecond,timeCentiseconds,lapMinute,lapSecond,lapCentiseconds;
+    hideShow('#startButton', '#lapButton');
+    //click on Start button
+    $('#startButton').click(function (e) { 
+        //mode on
+        mode=1;
+        startAction();
+    });
     
-});    
+    //hideShow() funtion   
+    function hideShow(x, y) {
+        $('.control').hide();
+        $(x).show();
+        $(y).show();
+    }
+
+    //startAction()  funtion 
+    function startAction()
+    {
+        action=setInterval(function(){
+            timeCounter++;
+            lapCounter++;
+             updateTime();
+        },10);
+
+
+    }
+    //updateTime() function
+    
+    function updateTime()
+    {
+      //  console.log(timeCounter);
+        //1min=60*100centiSeconds=6000centiSeconds
+        timeMinute=Math.floor(timeCounter/6000);  
+        timeSecond=Math.floor((timeCounter%6000)/100); 
+        timeCentiseconds=(timeCounter%6000)%100;
+        console.log(timeMinute);
+        console.log(timeSecond);     
+        console.log(timeCentiseconds);
+        $("#timeMinute").text(timeMinute); 
+        $("#timeSecond").text(timeSecond);
+        $("#timeCentisecond").text(timeCentiseconds);
+           //1min=60*100centiSeconds=6000centiSeconds
+           lapMinute=Math.floor(lapCounter/6000);       
+           lapSecond=Math.floor((lapCounter/6000)/100); 
+           lapCentiseconds=Math.floor((lapCounter/6000)%100);
+           $("#lapMinute").text(lapMinute); 
+           $("#lapSecond").text(lapSecond);
+           $("#lapCentisecond").text(lapCentiseconds);
+        }
 });
+
+
